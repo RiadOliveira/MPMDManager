7a5ea6670e5ed897ea793e04f0fc41ae87e074e3
Ríad Oliveira <riad.oliveira@hotmail.com>
2024-12-21 05:01:09 -0300

    Python Single_World abstraction fixed

--- src/single_world/python/abstraction/mpmd_manager.py
+++ src/single_world/python/abstraction/mpmd_manager.py
@@ -4,7 +4,7 @@ from .program_data import ProgramData
 from .filename_handler import get_filename
 
 class MPMDManager:
-  __local_comm = MPI.COMM_NULL
+  __local_data: ProgramData
   __manager_comm = MPI.COMM_NULL
   __programs_data: list[ProgramData] = []
 
@@ -12,93 +12,83 @@ class MPMDManager:
   def initialize():
     MPMDManager.__manager_comm = MPI.COMM_WORLD.Dup()
 
-    local_id = MPMDManager.__manager_comm.Get_attr(MPI.APPNUM)
-    MPMDManager.__set_local_comm(local_id)
-
-    local_rank = MPMDManager.__local_comm.Get_rank()
-    name_data = None if local_rank != 0 else get_filename()
-
+    local_name = get_filename()
     gathered_names = MPMDManager.__manager_comm.allgather(
-      name_data
+      local_name
     )
-    MPMDManager.__fill_programs_data(local_id, gathered_names)
+    MPMDManager.__set_programs_data(local_name, gathered_names)
 
   @staticmethod
-  def local_name(): return MPMDManager.__local_name
+  def local_name(): return MPMDManager.__local_data.name
 
   @staticmethod
-  def local_comm() -> MPI.Intracomm: return MPMDManager.__local_comm
+  def local_comm() -> MPI.Intracomm: return MPMDManager.__local_data.comm
   
   @staticmethod
-  def local_size(): return MPMDManager.__local_comm.Get_size()
+  def local_size(): return MPMDManager.__local_data.comm.Get_size()
 
   @staticmethod
   def intercomm_to(identifier: str | int):
-    return MPMDManager.__find_program_or_raise_exception(
-      identifier
-    ).intercomm
+    return MPMDManager.__find_program_or_error(identifier).comm
 
   @staticmethod
   def size_of(identifier: str | int):
-    return MPMDManager.__find_program_or_raise_exception(
-      identifier
-    ).size
+    return MPMDManager.__find_program_or_error(identifier).size
 
   @staticmethod
   def finalize():
     MPMDManager.__programs_data.clear()
 
-    if MPMDManager.__local_comm != MPI.COMM_NULL:
-      MPMDManager.__local_comm.Disconnect()
+    local_comm = MPMDManager.local_comm()
+    if local_comm != MPI.COMM_NULL: local_comm.Disconnect()
 
-    if MPMDManager.__manager_comm != MPI.COMM_NULL:
-      MPMDManager.__manager_comm.Disconnect()
-
-  @staticmethod
-  def __set_local_comm(local_id: int):
-    global_rank = MPMDManager.__manager_comm.Get_rank()
-
-    MPMDManager.__local_comm = MPMDManager.__manager_comm.Split(
-      local_id, global_rank
-    )
+    manager_comm = MPMDManager.__manager_comm
+    if manager_comm != MPI.COMM_NULL: manager_comm.Disconnect()
   
   @staticmethod
-  def __fill_programs_data(local_id: int, gathered_names: list[str | None]):
-    current_program: ProgramData | None = None
+  def __set_programs_data(local_name: str, gathered_names: list[str]):
+    MPMDManager.__set_local_data(local_name, gathered_names)
 
-    length = len(gathered_names)
-    rank, id_num = 0, -1
-    
+    previous_name: str | None = None
+    rank, length = 0, len(gathered_names)
+
     while rank < length:
       name = gathered_names[rank]
-
-      if name == None: current_program.increment_size()
-      else:
-        id_num += 1
-        
-        if id_num == local_id: 
-          MPMDManager.__local_name = name
-          rank += MPMDManager.local_size() - 1
-        else: current_program = MPMDManager.__add_program(id_num, name, rank)
-          
       rank += 1
 
+      if name == previous_name: 
+        MPMDManager.__programs_data[-1].increment_size()
+      else:
+        previous_name = name
+
+        if name == local_name: 
+          MPMDManager.__programs_data.append(MPMDManager.__local_data)
+          rank += MPMDManager.local_size() - 1
+        else: MPMDManager.__add_program(name, rank - 1)          
+
   @staticmethod
-  def __add_program(id_num: int, name: str, initial_rank: int):
+  def __set_local_data(local_name: str, gathered_names: list[str]):
+    global_rank = MPMDManager.__manager_comm.Get_rank()
+    local_id = gathered_names.index(local_name)
+
+    local_comm = MPMDManager.__manager_comm.Split(local_id, global_rank)
+    local_size = local_comm.Get_size()
+    MPMDManager.__local_data = ProgramData(local_name, local_comm, local_size)
+
+  @staticmethod
+  def __add_program(name: str, initial_rank: int):
     intercomm = MPMDManager.__get_program_intercomm(initial_rank)
-    current_program = ProgramData(id_num, name, intercomm)
-    MPMDManager.__programs_data.append(current_program)
+    MPMDManager.__programs_data.append(ProgramData(name, intercomm))
 
-    return current_program
-
+  
   @staticmethod
   def __get_program_intercomm(remote_program_initial_rank: int):
-    return MPMDManager.__local_comm.Create_intercomm(
-      0, MPMDManager.__manager_comm, remote_program_initial_rank, 0
+    return MPMDManager.local_comm().Create_intercomm(
+      0, MPMDManager.__manager_comm, remote_program_initial_rank
     )
   
   @staticmethod
-  def __find_program_or_raise_exception(identifier: str | int):
+  def __find_program_or_error(identifier: str | int):
     invalid_identifier = not isinstance(identifier, (str, int))
     if invalid_identifier: raise Exception("Invalid program identifier!")
 
@@ -109,15 +99,15 @@ class MPMDManager:
 
   @staticmethod
   def __find_program(identifier: str | int):
-    is_id_num = isinstance(identifier, int)
+    is_ind = isinstance(identifier, int)
 
-    if is_id_num: return MPMDManager.__find_program_by_id_num(identifier)
+    if is_ind: return MPMDManager.__find_program_by_ind(identifier)
     return MPMDManager.__find_program_by_name(identifier)
   
   @staticmethod
-  def __find_program_by_id_num(id_num: int):
-    for data in MPMDManager.__programs_data: 
-      if id_num == data.id_num: return data
+  def __find_program_by_ind(ind: int):
+    invalid_ind = ind < 0 or ind >= len(MPMDManager.__programs_data)
+    return None if invalid_ind else MPMDManager.__programs_data[ind]
   
   @staticmethod
   def __find_program_by_name(name: str):
--- src/single_world/python/abstraction/program_data.py
+++ src/single_world/python/abstraction/program_data.py
@@ -1,24 +1,19 @@
 from mpi4py import MPI
 
 class ProgramData:
-  __intercomm: MPI.Intercomm = MPI.COMM_NULL
-
-  def __init__(self, id_num: int, name: str, intercomm: MPI.Intercomm):
-    self.__id_num, self.__name, self.__intercomm, self.__size = id_num, name, intercomm, 1
+  def __init__(self, name: str, comm: MPI.Comm, size = 1):
+    self.__name, self.__comm, self.__size = name, comm, size
 
   def disconnect_comm(self):
-    if self.__intercomm != MPI.COMM_NULL: self.__intercomm.Disconnect()
+    if self.__comm != MPI.COMM_NULL: self.__comm.Disconnect()
 
   def __del__(self): self.disconnect_comm()
-
-  @property
-  def id_num(self): return self.__id_num
 
   @property
   def name(self): return self.__name
 
   @property
-  def intercomm(self): return self.__intercomm
+  def comm(self): return self.__comm
 
   @property
   def size(self): return self.__size
