086ca52117a213730c08070156d9aac3ceff0602
Ríad Oliveira <riad.oliveira@hotmail.com>
2024-12-28 00:16:13 -0300

    Multi_World C examples structure

--- src/multi_world/c/example/first.c
+++ src/multi_world/c/example/first.c
@@ -1,11 +1,36 @@
+#include "libClientManager.h"
 #include "libServerManager.h"
+#include "message.h"
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
-  const ServerManager* manager = Server_Init(argv, NULL, 2);
 
-  Server_Open(manager);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  Server_Finalize(manager);
+  Message message;
+  MPI_Datatype MessageType;
+  getMessageType(&MessageType);
+
+  if(rank == 0) {
+    const ServerManager* manager = Server_Init(argv, NULL, 2);
+    Server_Open(manager);
+    const MPI_Comm* secondComm = Server_Accept(manager, NULL);
+
+    fillMessage(&message, "0|First  to 1|Second", 1021);
+    MPI_Send(&message, 1, MessageType, 1, 0, *secondComm);
+    printMessage("(Send) 0|First  -> 1|Second:", &message);
+
+    Server_Finalize(manager);
+  } else if(rank == 1) {
+    const ClientManager* manager = Client_Init();
+    const MPI_Comm* thirdComm = Client_Connect(manager, "third0", NULL);
+
+    MPI_Recv(&message, 1, MessageType, 0, 0, *thirdComm, MPI_STATUS_IGNORE);
+    printMessage("(Recv) 1|First  <- 0|Third: ", &message);
+
+    Client_Finalize(manager);
+  }
+
   MPI_Finalize();
 }
--- src/multi_world/c/example/third.c
+++ src/multi_world/c/example/third.c
@@ -1,9 +1,36 @@
+#include "libClientManager.h"
 #include "libServerManager.h"
+#include "message.h"
 
 int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
-  const ServerManager* manager = Server_Init(argv, NULL, 2);
 
-  Server_Finalize(manager);
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  Message message;
+  MPI_Datatype MessageType;
+  getMessageType(&MessageType);
+
+  if(rank == 0) {
+    const ServerManager* manager = Server_Init(argv, NULL, 2);
+    Server_Open(manager);
+    const MPI_Comm* firstComm = Server_Accept(manager, NULL);
+
+    fillMessage(&message, "0|Third  to 1|First", 3011);
+    MPI_Send(&message, 1, MessageType, 1, 0, *firstComm);
+    printMessage("(Send) 0|Third  -> 1|First: ", &message);
+
+    Server_Finalize(manager);
+  } else if(rank == 1) {
+    const ClientManager* manager = Client_Init();
+    const MPI_Comm* secondComm = Client_Connect(manager, "second0", NULL);
+
+    MPI_Recv(&message, 1, MessageType, 0, 0, *secondComm, MPI_STATUS_IGNORE);
+    printMessage("(Recv) 1|Third  <- 0|Second:", &message);
+
+    Client_Finalize(manager);
+  }
+
   MPI_Finalize();
 }
